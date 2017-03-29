616ec86566a1e759ef3629faebcc391c98bf0651
Simon Schick <simonsimcity@gmail.com>
2017-03-29 15:15:46 +0400

    Updated the code to meet the eslint requirements

--- src/client/index.js
+++ src/client/index.js
@@ -1,29 +1,24 @@
-/* global Meteor, Mongo */
+/* global Meteor, Mongo, Package */
 
 const client = new Mongo.Collection('PublishJoin');
 
 client.get = function get(name) {
-	const join = this.findOne({
-	  _id: name,
-	});
+  const join = this.findOne({
+    _id: name,
+  });
 
-	return join && join.value;
+  return join && join.value;
 };
 
 client.has = function get(name) {
-	return !!this.findOne({
-	  _id: name,
-	});
+  return !!this.findOne({
+    _id: name,
+  });
 };
 
 if (Package.templating) {
-  Package.templating.Template.registerHelper('getPublishedJoin', function(name) {
-    return client.get(name);
-  });
-
-  Package.templating.Template.registerHelper('hasPublishedJoin', function(name) {
-    return client.has(name);
-  });
+  Package.templating.Template.registerHelper('getPublishedJoin', name => client.get(name));
+  Package.templating.Template.registerHelper('hasPublishedJoin', name => client.has(name));
 }
 
 export default client;
