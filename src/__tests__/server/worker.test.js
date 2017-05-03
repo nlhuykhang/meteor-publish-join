71ce4b00e77326a93a8586d179ba3520c7bdafb0
Khang Nguyen-Le <nlhuykhang@gmail.com>
2017-05-03 21:02:38 +0700

    Disable unit test of worker

--- src/__tests__/server/worker.test.js
+++ src/__tests__/server/worker.test.js
@@ -1,13 +1,13 @@
 /* global before, after */
 
-import { expect } from 'chai';
-import sinon from 'sinon';
+// import { expect } from 'chai';
+// import sinon from 'sinon';
 
-import {
-  startPublishWorker,
-  stopPublishWorker,
-} from '../../server/worker';
-
-const { describe, it } = global;
-
-describe('worker function', () => {});
+// import {
+//   startPublishWorker,
+//   stopPublishWorker,
+// } from '../../server/worker';
+//
+// const { describe, it } = global;
+//
+// describe('worker function', () => {});
